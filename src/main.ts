2aa1c691586d050a13b66d99f863ebb14796014a
王相博 <7685139+pythonistaWong@user.noreply.gitee.com>
2020-11-07 11:17:40 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
+/*
+ * @Author: your name
+ * @Date: 2020-10-24 16:00:59
+ * @LastEditTime: 2020-10-24 16:03:54
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /ts-demo/src/main.ts
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount('#app')
